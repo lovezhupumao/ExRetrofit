e61dac57790e4024b99d99763e86be73b201be95
ZhuPumao <20103177@cqu.edu.cn>
2016-05-22 16:18:47 +0800

    Initial commit

---
+++ app/src/main/java/train/zpm/com/exretrofit/MainActivity.java
@@ -0,0 +1,104 @@
+package train.zpm.com.exretrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mtext;
+    private EditText meditview;
+    private Button   mBtn;
+    private String key="2f121eb8bf260e938df638ec3cc2e5d4";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mtext=(TextView)findViewById(R.id.mytext);
+        meditview=(EditText)findViewById(R.id.myedit);
+        mBtn=(Button)findViewById(R.id.btn);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Retrofit retrofit=new Retrofit.Builder()
+                        .baseUrl("http://apis.juhe.cn/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                Phone phone=retrofit.create(Phone.class);
+                final Call<HaoMa> call=phone.getHaoMa(meditview.getText().toString(),key);
+
+
+                   new Thread(){
+                       @Override
+                       public void run() {
+                           try {
+                               Response<HaoMa> bodyResponse = call.execute();
+                               final String body = bodyResponse.body().getResult().getCity();//获取返回体的字符串
+                               MainActivity.this.runOnUiThread(new Runnable() {
+                                   @Override
+                                   public void run() {
+                                       mtext.setText(body);
+                                   }
+                               });
+                           }
+                           catch (IOException e){
+                               e.printStackTrace();
+                           }
+                       }
+                   }.start();
+
+              /*   call.enqueue(new Callback<HaoMa>() {
+                    @Override
+                    public void onResponse(Call<HaoMa> call, Response<HaoMa> response) {
+                        mtext.setText(response.body().getResult().getCity());
+                    }
+
+                    @Override
+                    public void onFailure(Call<HaoMa> call, Throwable t) {
+                        mtext.setText(t.getMessage());
+                    }
+                });*/
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/train/zpm/com/exretrofit/Phone.java
@@ -0,0 +1,13 @@
+package train.zpm.com.exretrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by Administrator on 2016/5/22 0022.
+ */
+public interface Phone {
+    @GET("mobile/get")
+    Call<HaoMa> getHaoMa(@Query("phone") String phone,@Query("key") String key);
+}
